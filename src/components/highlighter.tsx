6b359239a82371e0223b4803181ead80263c2d18
ayoub <ayoubaitelaouad@gmail.com>
2022-08-03 14:07:48 +0000

    backup commit

--- src/components/highlighter.tsx
+++ src/components/highlighter.tsx
@@ -2,20 +2,32 @@
 const move: any = (spot: string, dir: 'up' | 'down' | 'right' | 'left', amount: number) => {
   let x = 'abcdefgh';
   let y = '12345678';
-  let newSpot = '';
+  let newSpot = spot;
+  let index = 0;
   for (let i = 0; i < amount; i++){
     switch (dir){
       case 'up':
-        let index = y.indexOf(spot[0]);
-        newSpot = y[index + (index+1 < 8?1:0)];
+        index = y.indexOf(newSpot[0]);
+        newSpot = y[index + (parseInt(newSpot[0]) + 1 < 8?1:0)] + spot[1];
         break;
       case 'down':
-        let index = y.indexOf(spot[0]);
-        newSpot = y[index + (index+1 < 8?1:0)];
+        index = y.indexOf(newSpot[0]);
+        newSpot = y[index - (parseInt(newSpot[0]) - 1 < 0?1:0)] + spot[1];
+        break;
+      case 'right':
+        index = x.indexOf(newSpot[1]);
+        newSpot = newSpot[0] + x[index + (index + 1 < 8? 1 : 0)];
+        break;
+      case 'left':
+        index = x.indexOf(newSpot[1]);
+        newSpot = newSpot[0] + x[index - (index - 1 < 0? 1 : 0)];
         break;
     }
   }
+  return newSpot;
 }
+
+move('1a', 'right', 3);
 
 const Highlighter = (id: string, State: object) => {
   
